d9ec730d66b8104b5b95f5adb130b50b9f46f0e2
Salma ELFADILI <salmaelfadili10@gmail.com>
2024-10-20 22:18:39 +0200

    enregistrement historique des opérations

--- calculatrice.py
+++ calculatrice.py
@@ -2,6 +2,34 @@
 def addition(a, b):
     return a + b
 
-resultat = addition(5, 3)
+
+def soustraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        return "Division par zéro impossible!"
+    else:
+        return a / b
+a = float(input("Entrez le premier nombre : "))
+b = float(input("Entrez le deuxième nombre : "))
+operation = input("Entrez l'opération (+, -, *, /) : ")
+
+if operation == "+":
+    resultat = addition(a, b)
+elif operation == "-":
+    resultat = soustraction(a, b)
+elif operation == "*":
+    resultat = multiplication(a, b)
+elif operation   == "/":
+    resultat = division(a, b)
+else:
+    resultat = "Opération invalide !"
 print(f"Le résultat est : {resultat}")
+
+
+    
 
